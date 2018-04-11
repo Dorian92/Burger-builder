f2211634e9a92fd1f18dce20a71f980e5900b97d
Dorian Sabo <dorian.sabo@brightmarbles.io>
2018-04-11 08:23:33 +0100

    Implementing the Backdrop Component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -78,6 +78,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     }
 
+    purchaseCancleHandler = () => {
+        this.setState({purchasing: false})
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -88,7 +92,7 @@ class BurgerBuilder extends Component {
         }
         return (
             <Auxx>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancleHandler}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
