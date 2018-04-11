eae1a56cd3a3226eb09781634dc5c041ac4cdaef
Dorian Sabo <dorian.sabo@brightmarbles.io>
2018-04-11 13:19:18 +0100

    Improving Performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxx from '../../../hoc/Auxx';
 import Button from '../../UI/Button/Button.js';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map((igKey,index) => {
-            return <li key={index}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}</li>
-        })
-    return (
-        <Auxx>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout</p>
-            <Button btnType={"Danger"} clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType={"Success"} clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Auxx>
-    );
-};
+class OrderSummary extends Component {
+    componentWillMount() {
+        console.log('[OrderSummary] WillUpdate');
+    }
 
-export default orderSummary;
+    
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map((igKey,index) => {
+            return <li key={index}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
+        })
+
+        return(
+            <Auxx>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout</p>
+                <Button btnType={"Danger"} clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType={"Success"} clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Auxx>
+        );
+    }
+}
+
+export default OrderSummary;
