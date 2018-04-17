608cb5ce94c1e592e4f72164f22955a0ead6b4b0
Dorian Sabo <dorian.sabo@brightmarbles.io>
2018-04-17 11:05:11 +0100

    Creating a Custom Dynamic Input Component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spiner/Spiner';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -48,11 +49,11 @@ class ContactData extends Component {
     render() {
         let from = (
             <form>
-            <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-            <input className={classes.Input} type="text" name="email" placeholder="Your Email" />
-            <input className={classes.Input} type="text" name="street" placeholder="Street" />
-            <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-            <Button clicked={this.orderHandler} btnType="Success">ORDER</Button>
+                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+                <Input inputtype="input" type="text" name="email" placeholder="Your Email" />
+                <Input inputtype="input" type="text" name="street" placeholder="Street" />
+                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+                <Button clicked={this.orderHandler} btnType="Success">ORDER</Button>
         </form>
         );
         if (this.state.loading) {
