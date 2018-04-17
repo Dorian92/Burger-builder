a54f9686f6c0cb0d0b8c20d488849113362ffef7
Dorian Sabo <dorian.sabo@brightmarbles.io>
2018-04-17 15:24:17 +0100

    Adding Validation Feedback

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,16 +3,30 @@ import React from 'react';
 import classes from './Input.css';
 
 const input = (props) => {
-    let inputElement = null
+    let inputElement = null;
+    const inputClasses = [classes.inputElement];
+
+    if(props.invalid && props.shouldValidate) {
+        inputClasses.push(classes.Invalid);
+    }
+
     switch (props.elementType) {
         case ('input'):
-            inputElement = <input onChange={props.changed}  className={classes.inputElement} {...props.elementConfig}  value={props.value} />;
+            inputElement = <input 
+                onChange={props.changed}  
+                className={inputClasses.join(' ')} 
+                {...props.elementConfig}  
+                value={props.value} />;
             break;
         case ('textarea'):
-            inputElement = <textarea onChange={props.changed}  className={classes.inputElement} {...props.elementConfig} value={props.value} />;
+            inputElement = <textarea 
+                onChange={props.changed}  
+                className={inputClasses} 
+                {...props.elementConfig} 
+                value={props.value} />;
             break;
         case ('select'):
-            inputElement = (<select onChange={props.changed}  className={classes.inputElement} value={props.value}>
+            inputElement = (<select onChange={props.changed}  className={inputClasses} value={props.value}>
                 {props.elementConfig.options.map(option => (
                     <option key={option.value} value={option.value}>
                         {option.displayValue}
@@ -21,7 +35,7 @@ const input = (props) => {
             </select>);
             break;
         default: 
-            inputElement = <input onChange={props.changed}  className={classes.inputElement} {...props.elementConfig} value={props.value} />;
+            inputElement = <input onChange={props.changed}  className={inputClasses} {...props.elementConfig} value={props.value} />;
     }
     return (
         <div className={classes.Input}>
