7483c4bb6dd3844cb7e6ac197dfd3572034872cc
Dorian Sabo <dorian.sabo@brightmarbles.io>
2018-04-25 10:37:27 +0200

    Adding Actions

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,3 +2,5 @@ export { addIngredient, remveIngredient, initIngredients } from './burgerBuilder
 
 
 export { purchaseBurger , purchaseInit, fetchOrders} from './order';
+
+export { auth } from './auth';
